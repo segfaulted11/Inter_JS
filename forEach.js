7300dc9a29ae9a2b9705ff0df25266065e6cde1c
Kazi Rakin Bin Zaman <segfaulted11@gmail.com>
2026-03-06 18:14:34 +0000

    janina bhai

---
+++ forEach.js
@@ -0,0 +1,16 @@
+//  forEach is not a loop. its an array method. it can print all the elements of an array.
+
+let array = ["A","B","C","D","E"]
+
+array.forEach((element,index)=>{
+    console.log(element+" YO "+index);
+})
+/* output ->
+A YO 0
+B YO 1
+C YO 2
+D YO 3
+E YO 4
+*/
+
+// here, the forEach function(or method) is a higher order function because a function is passed as an argument to it. and the passed function is called a callback function. this is also a anonymous function (since it doesnt have a name) and an arrow function.
---
+++ mapMethod.js
@@ -0,0 +1,7 @@
+// map() method accepts a callback function and applies that function to each element of of an array, and then it returns a new array. this is similar to forEach method. however, forEach doesnt return a new array.
+
+let array = [1,2,3,4,5]
+
+array.map((elements)=>{
+    console.log(ele)
+})
